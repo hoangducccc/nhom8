dc6a6a5cded766feb2a6e6711e637a20eb9590de
Hoa Vu <hoa.vm203708@sis.hust.edu.vn>
2023-02-11 11:22:44 +0700

    fix loi font chu

--- tb-yte/modules/home/main.php
+++ tb-yte/modules/home/main.php
@@ -5,27 +5,6 @@ ob_start();
 global $list_product_cat;
 $list_cat = $list_product_cat;
 ?>
-<!doctype html>
-<html class="no-js" lang="zxx">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>NHÓM 08 - KTUDPM</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="assets\img\favicon.png">
-
-        <!-- all css here -->
-        <link rel="stylesheet" href="assets\css\bootstrap.min.css">
-        <link rel="stylesheet" href="assets\css\plugin.css">
-        <link rel="stylesheet" href="assets\css\bundle.css">
-        <link rel="stylesheet" href="assets\css\style.css">
-        <link rel="stylesheet" href="assets\css\responsive.css">
-        <script src="assets\js\vendor\modernizr-2.8.3.min.js"></script>
-    </head>
-    <body>
-        <!-- Add your site or application content here -->
 <?php
 get_header();
 ?>
